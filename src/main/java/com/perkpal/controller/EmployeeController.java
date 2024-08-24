a02d2cb884545ebe4071d04a6557c59b5c618b7a
RithikAR25 <arikothanrithik@gmail.com>
2024-08-24 16:13:08 +0530

    sso done

--- src/main/java/com/perkpal/controller/EmployeeController.java
+++ src/main/java/com/perkpal/controller/EmployeeController.java
@@ -1,46 +1,46 @@
-package com.perkpal.controller;
+    package com.perkpal.controller;
 
-import com.perkpal.dto.EmployeeRoleDto;
-import com.perkpal.dto.EmployeeUpdatePointsDto;
-import com.perkpal.response.ResponseHandler;
-import com.perkpal.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import com.perkpal.dto.EmployeeRoleDto;
+    import com.perkpal.dto.EmployeeUpdatePointsDto;
+    import com.perkpal.response.ResponseHandler;
+    import com.perkpal.service.EmployeeService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import static com.perkpal.constants.Message.*;
+    import static com.perkpal.constants.Message.*;
 
-@RestController
-@RequestMapping("/api/v1/employee")
-public class EmployeeController {
-    @Autowired
-    private EmployeeService employeeService;
+    @RestController
+    @RequestMapping("/api/v1/employee")
+    public class EmployeeController {
+        @Autowired
+        private EmployeeService employeeService;
 
-    @GetMapping
-    public ResponseEntity<Object> getAccounts() {
-        return ResponseHandler.responseBuilder(REQUESTED_EMPLOYEE_DETAILS,
-                HttpStatus.OK, employeeService.getEmployees());
+        @GetMapping
+        public ResponseEntity<Object> getAccounts() {
+            return ResponseHandler.responseBuilder(REQUESTED_EMPLOYEE_DETAILS,
+                    HttpStatus.OK, employeeService.getEmployees());
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Object> getAccountWithId(@PathVariable(name = "id") Long id) {
+            return ResponseHandler.responseBuilder(REQUESTED_EMPLOYEE_DETAILS, HttpStatus.OK, employeeService.getEmployeeById(id));
+        }
+
+        @PutMapping("/{id}/points")
+        public ResponseEntity<Object> updateEmployeePoints(@PathVariable(name = "id") Long id,
+                                                           @RequestBody EmployeeUpdatePointsDto employeeUpdatePointsDto) {
+            return ResponseHandler.responseBuilder(EMPLOYEE_POINTS_UPDATION , HttpStatus.OK,
+                    employeeService.updateEmployeePoints(id, employeeUpdatePointsDto));
+        }
+        @GetMapping("/{id}/get-points")
+        public ResponseEntity<Object> getAccountPointsWithId(@PathVariable(name = "id") Long id) {
+            return ResponseHandler.responseBuilder(REQUESTED_EMPLOYEE_DETAILS, HttpStatus.OK, employeeService.getEmployeePointsById(id));
+        }
+        @GetMapping("/role")
+        public ResponseEntity<Object> getRoleByEmail(@RequestParam(name = "email") String email) {
+            EmployeeRoleDto employeeRoleDto = employeeService.getRoleByEmail(email);
+            return ResponseHandler.responseBuilder(EMPLOYEE_ROLE_RETRIEVED, HttpStatus.OK, employeeRoleDto);
+        }
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getAccountWithId(@PathVariable(name = "id") Long id) {
-        return ResponseHandler.responseBuilder(REQUESTED_EMPLOYEE_DETAILS, HttpStatus.OK, employeeService.getEmployeeById(id));
-    }
-
-    @PutMapping("/{id}/points")
-    public ResponseEntity<Object> updateEmployeePoints(@PathVariable(name = "id") Long id,
-                                                       @RequestBody EmployeeUpdatePointsDto employeeUpdatePointsDto) {
-        return ResponseHandler.responseBuilder(EMPLOYEE_POINTS_UPDATION , HttpStatus.OK,
-                employeeService.updateEmployeePoints(id, employeeUpdatePointsDto));
-    }
-    @GetMapping("/{id}/get-points")
-    public ResponseEntity<Object> getAccountPointsWithId(@PathVariable(name = "id") Long id) {
-        return ResponseHandler.responseBuilder(REQUESTED_EMPLOYEE_DETAILS, HttpStatus.OK, employeeService.getEmployeePointsById(id));
-    }
-    @GetMapping("/role")
-    public ResponseEntity<Object> getRoleByEmail(@RequestParam(name = "email") String email) {
-        EmployeeRoleDto employeeRoleDto = employeeService.getRoleByEmail(email);
-        return ResponseHandler.responseBuilder(EMPLOYEE_ROLE_RETRIEVED, HttpStatus.OK, employeeRoleDto);
-    }
-}
