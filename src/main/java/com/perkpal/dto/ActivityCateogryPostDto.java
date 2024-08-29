c6624d7200101e5e75ecf6516db08823d899f39b
RithikAR25 <arikothanrithik@gmail.com>
2024-08-29 18:22:29 +0530

    export data

--- src/main/java/com/perkpal/dto/ActivityCateogryPostDto.java
+++ src/main/java/com/perkpal/dto/ActivityCateogryPostDto.java
@@ -1,9 +1,12 @@
 package com.perkpal.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ActivityCateogryPostDto {
+
+    @NotBlank(message = "Activity name is required.")
     private String activityName;
     private Long categoryId;          // Optional: Use this if you're linking to an existing category
     private String categoryName;      // Include this if creating a new category
--- src/main/java/com/perkpal/dto/CategoryActivityDto.java
+++ src/main/java/com/perkpal/dto/CategoryActivityDto.java
@@ -6,5 +6,5 @@ import lombok.Data;
 public class CategoryActivityDto {
     private Long id;
     private String activityName;
-    private int weightagePerHour;
+    private int  weightagePerHour;
 }
