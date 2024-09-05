e2f3ee552bef55fe79481d7d348a46833bef3fd6
NairChetan <sanjaynair8192@gmail.com>
2024-09-05 10:54:39 +0530

    added clubId to ClubDto

--- src/main/java/com/perkpal/dto/ClubDto.java
+++ src/main/java/com/perkpal/dto/ClubDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClubDto {
+    private int clubId;
     private Long createdBy;
     private String clubName;
     private String clubDescription;
